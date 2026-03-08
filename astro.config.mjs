d9ddcc1554f42cd2f5cc97a5be123d9c928f6b67
Jose Javier <jjzoto@gmail.com>
2026-03-08 16:29:36 -0500

    chore(config): disable astro dev toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
   output: 'static',
   adapter: cloudflare(),
   integrations: [svelte(), sitemap()],
+  devToolbar: { enabled: false },
 
   vite: {
     plugins: [tailwindcss()]
